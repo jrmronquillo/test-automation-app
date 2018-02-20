2a3ba5f599eead4f07ec3e67a1a82139a2def4f8
Jerome Ronquillo <jronquillo@directv.com>
2018-02-20 13:23:44 -0800

    add to script, open scoreguide and navigate menu

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ rack = "A03"
 slot = "1-16"
 
 def main():
+	print stbt.ocr()
 	keySend.keyPressAPI(rack, "exit", slot)
 	time.sleep(1)
 	# tune to sports mix
@@ -43,18 +44,37 @@ def main():
 	
 	# return to mix channel
 	keySend.tune("205", rack, slot)
+	keySend.keyPressAPI(rack, 'exit', slot)
+	time.sleep(2)
+	# internet connected, verify scoreguide
 	
-	# open app
 	keySend.keyPressAPI(rack, "red", slot)
+	
 
+	# detect combo mix
 	keySend.keyPressAPI(rack, "downArrow", slot)
 	stbt.wait_for_match('images/scoreguide_highlight.png')
-	print "test file executed"
+	keySend.keyPressAPI(rack, "select", slot)
+	stbt.wait_for_match('images/scoreguide_banner_hd.png')
+	
+	# verify scoreguide highlights
+	stbt.wait_for_match('images/nba_highlight.png')
+	keySend.keyPressAPI(rack, "downArrow", slot)
+	print "test output:"+str(image_verified('images/scoreguide_minimize_highlight.png', 3))
+	while not image_verified('images/scoreguide_minimize_gray.png', 3):
+		keySend.keyPressAPI(rack, "downArrow", slot)
+	
+	print "test file executed and completed"
 
 
 def verify_cell_highlight():
-	stbt.wait_for_match('images/blue_cell_vertical.png')
-	stbt.wait_for_match('images/blue_cell_horizontal.png')
+	#2 cells
+	#stbt.wait_for_match('images/blue_cell_vertical.png')
+	#stbt.wait_for_match('images/blue_cell_horizontal.png')
+	
+	#4 cells
+	stbt.wait_for_match('images/blue_cell_highlight_vertical_4_cell.png')
+	stbt.wait_for_match('images/blue_cell_highlight_horizontal_4_cell.png')
 	print "blue cells detected"
 
 def verify_channel_tune():
@@ -62,6 +82,21 @@ def verify_channel_tune():
 	stbt.wait_for_match('images/info_banner_corner.png')
         keySend.keyPressAPI(rack, "prev", slot)
         time.sleep(6)
+
+def image_verified(image, threshold):
+	counter = 0
+        for x in range(0, threshold):
+		if stbt.detect_match(image).next().match:
+			print "Detected True"
+			counter += 1
+		else:
+			print "detected false"
+	print "counter:"+str(counter)
+	if counter < threshold:
+		print "checked "+str(threshold)+"times did not find strong  match for image"
+		return False
+	else:
+		return True
 	
 
 if __name__ == '__main__':
