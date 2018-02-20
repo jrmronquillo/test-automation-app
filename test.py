7923377e000012c081fb081913cfec2d117ef0d5
Jerome Ronquillo <jronquillo@directv.com>
2018-02-20 10:20:09 -0800

    refactored test.py to be idiomatic so that functions can be declared anywhere

--- test.py
+++ test.py
@@ -5,18 +5,33 @@ import time
 rack = "A03"
 slot = "1-16"
 
-# tune api test
-# keySend.tune("500", rack, slot)
+def main():
+	keySend.keyPressAPI(rack, "exit", slot)
+	time.sleep(1)
+	# tune to sports mix
+	keySend.tune("205", rack, slot)
 
-keySend.keyPressAPI(rack, "exit", slot)
-time.sleep(1)
-# tune to sports mix
-keySend.tune("205", rack, slot)
-# open app
-keySend.keyPressAPI(rack, "red", slot)
+	# verify snipe red button
+	stbt.wait_for_match('images/red_button.png')
 
-keySend.keyPressAPI(rack, "downArrow", slot)
-stbt.wait_for_match('images/scoreguide_highlight.png')
-print "test file executed"
+	# verify blue cells
+	verify_cell_highlight()
+
+	# verify higlights can navigate to all cells
+	keySend.keyPressAPI(rack, "rightArrow", slot)
+
+	# open app
+	keySend.keyPressAPI(rack, "red", slot)
+
+	keySend.keyPressAPI(rack, "downArrow", slot)
+	stbt.wait_for_match('images/scoreguide_highlight.png')
+	print "test file executed"
 
 
+def verify_cell_highlight():
+	stbt.wait_for_match('images/blue_cell_vertical.png')
+	stbt.wait_for_match('images/blue_cell_horizontal.png')
+	print "blue cells detected"
+
+if __name__ == '__main__':
+	main()
